d1807ff49c48af73b80c21bf4303973536e63108
Melyssa Mariana <melyssamariana@discente.ufg.br>
2023-07-26 21:32:46 -0300

    changing ips

--- constMP.py
+++ constMP.py
@@ -1,7 +1,7 @@
 #To Do: Replace all this with a naming service
 PEERS = ['172.31.31.88','172.31.25.168','172.31.21.69',' 172.31.28.87','172.31.19.61','172.31.29.107']
-PORT = 4567
+PORT = 5679
 N = 6
 N_MSGS = 10
 SERVER_ADDR = '172.31.28.240'
-SERVER_PORT = 4568
+SERVER_PORT = 5679
